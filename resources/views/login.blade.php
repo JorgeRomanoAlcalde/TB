29c11ca1c521f05020a4001aa1e80b422938f534
Jorge <jorgealmacen25@gmail.com>
2024-05-22 18:09:26 +0200

    Actualizacion grafiac register

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,15 +7,16 @@
             <form method="POST" action="{{ route('inicia-sesion') }}">
                 @csrf
                 <div class="form-group">
-                  <label for="exampleInputEmail1">Nombre de usuario</label>
-                  <input type="email" class="form-control" placeholder="Introduce un nombre de usuario">
+                  <label for="nickname">Nombre de usuario</label>
+                  <input type="text" id="nickname" class="form-control" placeholder="Introduzca su nombre de usuario" required>
                 <div class="form-group" style="margin-bottom: 20px">
-                  <label for="exampleInputPassword1">Contraseña</label>
-                  <input type="password" class="form-control" placeholder="Introduce una contraseña">
+                  <label for="password">Contraseña</label>
+                  <input type="password" id="password" class="form-control" placeholder="Introduzca su contraseña" required minlength="8">
                   <small id="emailHelp" class="form-text text-muted">La longitud minima es de 8 carácteres</small>
                 </div>
                 </div>
                 <button type="submit" class="btn btn-primary botonFormulario">Iniciar Sesión</button>
+                <p>¿No tiene cuenta? <a href="{{ route('registro') }}">Regístrese</a></p>
               </form>
         </div>
     </section>
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,6 +1,27 @@
 @extends('master')
 @section('index')
-    <section class="contenedorFormulario">
+<section class="contenedorFormulario">
+    <div class="formulario">
+        <img src="{{ asset('images\iconBlue.png') }}" alt="TRACE BETTER Icon">
+        <h1>Regístrese</h1>
+        <form method="POST" action="{{ route('inicia-sesion') }}">
+            @csrf
+            <div class="form-group">
+              <label for="nickname">Nombre de usuario</label>
+              <input type="text" id="nickname" class="form-control" placeholder="Introduzca un nombre de usuario" required>
+            <div class="form-group" style="margin-bottom: 20px">
+              <label for="password">Contraseña</label>
+              <input type="password" id="password" class="form-control" placeholder="Introduzca una contraseña" required minlength="8">
+              <small id="emailHelp" class="form-text text-muted">La longitud minima es de 8 carácteres</small>
+            </div>
+            </div>
+            <button type="submit" class="btn btn-primary botonFormulario">Registrarse</button>
+            <p>¿Ya tiene cuenta? <a href="{{ route('login') }}">Inicie sesión</a></p>
+          </form>
+    </div>
+</section>
+
+     <!--<section class="contenedorFormulario">
         <h1>Registrese</h1>
         <form method="POST" action="{{ route('validar-registro') }}">
             @csrf
@@ -13,18 +34,18 @@
             <label for="password">Contraseña:</label><br>
             <input type="password" id="password" name="password"><br>
 
-            <!--<input type="checkbox" id="admin" name="admin">
+           <input type="checkbox" id="admin" name="admin">
         <label for="admin">Usuario administrador</label><br>
         <label for="name">Nombre:</label><br>
         <input type="text" id="name" name="name"><br>
         <label for="surname">Apellidos:</label><br>
         <input type="text" id="surname" name="surname"><br>
         <label for="phone">Teléfono:</label><br>
-        <input type="tel" id="phone" name="phone"><br>*/ }} -->
+        <input type="tel" id="phone" name="phone"><br>*/ }} 
 
             <input type="submit" value="Enviar">
             <p>Ya tiene cuenta <a href="{{ route('login') }}">Inicie sesión</a></p>
 
         </form>
-    </section>
+    </section>-->
 @endsection
